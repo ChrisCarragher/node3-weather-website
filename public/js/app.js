ad8a96f860e7dd47fbd3496966abc794745ce03c
Christopher <14chris2011@gmail.com>
2021-02-12 17:58:14 -0800

    Rm unnecessart console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,6 @@ const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const msg1 = document.querySelector('#msg1')
 const msg2 = document.querySelector('#msg2')
-
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
     const location = search.value
