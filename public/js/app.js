94e0e8448b61ee30535410a904933735e8179eb4
Christopher Carragher <christopher.carragher2@mail.dcu.ie>
2021-02-12 18:44:34 -0800

    Test

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,7 @@ const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const msg1 = document.querySelector('#msg1')
 const msg2 = document.querySelector('#msg2')
+
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
     const location = search.value
